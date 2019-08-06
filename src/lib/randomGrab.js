dca0873dc087a71515a5bb92c47d97137d9cd5e0
dzucconi <mail@damonzucconi.com>
2019-08-05 21:24:56 -0400

    Avoids depleting the pool

--- src/lib/randomGrab.js
+++ src/lib/randomGrab.js
@@ -2,7 +2,7 @@ import { knuthShuffle as shuffle } from "knuth-shuffle";
 
 export const randomGrab = (previous, xs) => {
   let next = previous;
-  const pool = shuffle(xs);
+  const pool = shuffle([...xs]);
 
   while (next.Id === previous.Id) {
     next = pool.pop();
