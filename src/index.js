a1c3dd38bc5898c39cf0e22c2c9794a3b5a53421
dzucconi <mail@damonzucconi.com>
2019-09-25 08:57:24 -0400

    Upgrades dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import audiate from "audiate";
+import { block } from "audiate";
 
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-audiate({
+block({
   onEnable: () => ReactDOM.render(<App />, document.getElementById("root"))
 });
 
